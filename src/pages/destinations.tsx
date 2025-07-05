6d8e05765ee016bf1af4b8a4c2c2276ce84fe7ff
nathansungu <nathanamudavi@gmail.com>
2025-07-05 14:41:33 +0300

    destination page

---
+++ src/pages/destinations.tsx
@@ -0,0 +1,18 @@
+import HandleNavbar from "../components/home.components/navbar";
+import HandleHeader from "../components/destination.components/header";
+import HandleCards from "../components/destination.components/cards";
+
+const Destination =()=>{
+    return(
+        <>
+        <HandleNavbar/>
+        <HandleHeader/>
+        <HandleCards/>
+        
+        
+        </>
+    )
+    
+}
+
+export default Destination;
