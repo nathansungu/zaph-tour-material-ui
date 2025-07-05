da5bc40b130e6ef7b3bb43e2db86f50f240ddb4c
nathansungu <nathanamudavi@gmail.com>
2025-07-05 14:42:18 +0300

    trips page

---
+++ src/pages/trip-types.tsx
@@ -0,0 +1,14 @@
+import HandleNavbar from "../components/home.components/navbar";
+import HandleTrips from "../components/trips.components/trips";
+const TripTypes =()=>{
+    return(
+        <>
+        <HandleNavbar/>
+        <HandleTrips/>
+        
+        </>
+    )
+    
+}
+
+export default TripTypes;
