601237e658aa4b1903d51428709d14aed78122d5
nathansungu <nathanamudavi@gmail.com>
2025-07-05 14:39:34 +0300

    trip types data

---
+++ src/data/triptypes.ts
@@ -0,0 +1,83 @@
+const trips = [
+  {
+    title: "Honeymoon",
+    description: "Indulge in a luxury all-inclusive Jamaica honeymoon at Zoetry Montego Ba, offering the finest in gourmet. ",
+    image: "/honeymoon.jpg",
+    gallery:[""],
+    price: 120000,
+    offer: 10
+  },
+  {
+    title: "Family",
+    description: "Fun-filled trips perfect for the whole family, with activities and comfort for all ages.",
+    image: "/family-1.jpg",
+    gallery:["/family-1.jpg", "/family-2.jpg"],
+    price: 95000,
+    offer: 0
+  },
+  {
+    title: "Adventure",
+    description: "Thrilling trips for adrenaline junkies from hiking and rafting to mountain biking.",
+    image: "/adventure.jpg",
+    gallery:["/adventure-2.jpg", "/adventure-3.jpg"],
+    price: 110000,
+    offer: 15
+  },
+  {
+    title: "Cultural Tours",
+    description: "Explore rich cultures, heritage sites, and local traditions in immersive tours.",
+    image: "/cultural-3.jpg",
+    gallery:["/caltural-3.jpg", ],
+    price: 85000,
+    offer: 0
+  },
+  {
+    title: "Wildlife Safaris",
+    description: "Experience the wild up close with expertly guided safaris across game reserves.",
+    image: "/wildlife.webp",
+    gallery:["/wildlife-2.wbp", "/wildlife-3.webp"],
+    price: 135000,
+    offer: 20
+  },
+  {
+    title: "Eco-Tours",
+    description: "Sustainable travel that lets you explore nature responsibly and impactfully.",
+    image: "/eco-tour-1.jpg",
+    price: 90000,
+    offer: 0
+  },
+  {
+    title: "Luxury Vacations",
+    description: "Top-tier travel with five-star accommodations, exclusive services, and indulgent experiences.",
+    image: "/luury-1.jpg",
+    gallery:["/luary-2.webp"],
+    price: 200000,
+    offer: 25
+  },
+  {
+    title: "Beach Holidays",
+    description: "Relax under the sun on pristine beaches with crystal-clear waters and golden sands.",
+    image: "/beach-2.jpg",
+    gallery:["/beach-1.jpg"],
+    price: 105000,
+    offer: 0
+  },
+  {
+    title: "Wellness Retreats",
+    description: "Holistic getaways for mind, body, and soul including yoga, meditation, and spa treatments.",
+    image: "/wellness-1.JPG",
+    gallery:["/wellness-4.jpeg","/wellness.JPG",""],
+    price: 115000,
+    offer: 12
+  },
+  {
+    title: "Culinary Tours",
+    description: "Savor flavors of the world with food-focused trips that include tastings, classes, and local chefs.",
+    image: "/culinary-2.jpg",
+    gallery:["/culinary.jpg", "culinary-2.jpg", "culinary-3.jpg"],
+    price: 95000,
+    offer: 0
+  }
+];
+
+export default trips;
