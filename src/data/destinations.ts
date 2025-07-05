8af6a5e990a2eba4bf29ca64cb37e5f96dc8e11a
nathansungu <nathanamudavi@gmail.com>
2025-07-05 14:39:01 +0300

    destination data

---
+++ src/data/destinations.ts
@@ -0,0 +1,58 @@
+const destinations = [
+  {
+    name: "Mt. Kenya",
+    image: "https://www.kenyasafari.com/images/kenya-hiking-trails-500x300.jpg?t=1751309691?t=1751309691",
+    description:
+      "A scenic mountain adventure with breathtaking views and alpine wildlife. Ideal for hikers seeking a rewarding challenge.",
+    price: "$750",
+    priceFor10: "$6,000",
+    rating: 5
+  },
+  {
+    name: "Mt. Ruwenzori",
+    image: "https://media.gettyimages.com/id/956958720/photo/waterfall-in-mobuku-valley-at-3900m-rwenzori-uganda-in-thick-fog-and-rain-giant-lobelias-and.jpg?s=612x612&w=0&k=20&c=ii5t3_sDXJKae5mEgRbqpHpGo0OH12dWbW9g_gc-66c=",
+    description:
+      "Explore Africa's mystical 'Mountains of the Moon' with guided trekking. A paradise for mountaineers and nature lovers.",
+    price: "$820",
+    priceFor10: "$6,560",
+    rating: 5
+  },
+  {
+    name: "Lake Nakuru",
+    image: "https://www.kws.go.ke/sites/default/files/2019-11/parks/Lake%20Nakuru/Lake-Nakuru-2.jpg",
+    description:
+      "Home to flamingos and rhinos, perfect for bird lovers and photographers. Enjoy peaceful moments by the lakeside.",
+    price: "$600",
+    priceFor10: "$4,800",
+    rating: 4
+  },
+  {
+    name: "Tsavo National Park",
+    image: "https://www.tsavonationalparkkenya.com/wp-content/uploads/2020/02/sa-4.jpg",
+    description:
+      "Vast landscapes, red elephants, and untamed wilderness await. One of Kenya's largest and wildest parks.",
+    price: "$900",
+    priceFor10: "$7,200",
+    rating: 5
+  },
+  {
+    name: "Nairobi National Park",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbTj_5sM2b5z_8IozLx-npPrDOJfZbAhv_Rg&s",
+    description:
+      "See wild animals against Nairobi’s skyline just minutes from the city. Great for a quick and exciting safari.",
+    price: "$400",
+    priceFor10: "$3,200",
+    rating: 5
+  },
+  {
+    name: "Coastal Kenya",
+    image: "https://www.tsavonationalparkkenya.com/wp-content/uploads/2022/05/istockphoto-655560272-612x612-1.jpg",
+    description:
+      "Relax on white-sand beaches with turquoise waters and Swahili culture. Perfect for a tropical getaway.",
+    price: "$850",
+    priceFor10: "$6,800",
+    rating: 5
+  }
+];
+
+export default destinations;
